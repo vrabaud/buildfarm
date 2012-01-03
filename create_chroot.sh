e49268c03c92d8a8604f32d834cde63d23028c97
Troy Straszheim <straszheim@willowgarage.com>
2012-01-03 07:23:03 -0800

    only buildd supported on lucid.  PITA that the farm is running such old stuff.

--- create_chroot.sh
+++ create_chroot.sh
@@ -10,7 +10,7 @@ sudo pbuilder --create \
     --distribution $DISTRO \
     --architecture $ARCH \
     --basetgz $BASETGZ \
-    --debootstrapopts --variant=$VARIANT \
+    --debootstrapopts --variant=buildd \
     --components "main universe multiverse" \
     --extrapackages "lsb-release ccache cmake libopenmpi-dev libboost-dev gccxml python-empy python-yaml python-setuptools openssl"
 
