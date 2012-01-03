804d9c5eb7392e6b1bf461253997465a1137569c
Troy Straszheim <straszheim@willowgarage.com>
2012-01-03 07:08:14 -0800

    dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TOP=$(cd `dirname $0` ; /bin/pwd)
+
 JOB_NAME=$1
 DISTRO=$2
 ARCH=$3
@@ -10,5 +12,5 @@ SHORTJOB=$(dirname $1)
 
 /bin/echo "SHORTJOB: $SHORTJOB"
 
-./$SHORTJOB.sh $DISTRO $ARCH
+$TOP/$SHORTJOB.sh $DISTRO $ARCH
 
