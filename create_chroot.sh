dd72fded9b075918a7fc3376e1492dde8fb0f72d
Troy Straszheim <straszheim@willowgarage.com>
2012-01-03 06:53:03 -0800

    makechroot

---
+++ create_chroot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DISTRO=$1
+ARCH=$2
+VARIANT=$3
+
+BASETGZ=$DISTRO-$ARCH-$VARIANT.tgz
+
+sudo pbuilder --create \
+    --distribution $DISTRO \
+    --architecture $ARCH \
+    --basetgz $BASETGZ \
+    --debootstrapopts --variant=$VARIANT \
+    --components "main universe multiverse" \
+    --extrapackages "lsb-release ccache cmake libopenmpi-dev libboost-dev gccxml python-empy python-yaml python-setuptools openssl"
+
