40cc255e4cb5bd9b49e79655ce8e377dc0b8beb9
Troy Straszheim <straszheim@willowgarage.com>
2012-01-04 11:10:05 -0800

    meh

--- catkin-install-per-stack.sh
+++ catkin-install-per-stack.sh
@@ -3,3 +3,21 @@
 echo "$*"
 
 ./buildfarm/sanity_check.sh
+
+/bin/echo "vvvvvvvvvvvvvvvvvvv  catkin-installed-per-stack.sh vvvvvvvvvvvvvvvvvvvvvv"
+
+if [ -z "$WORKSPACE" ] ; then
+    /bin/echo "Don't see no workspace."
+    exit 1
+fi
+
+cd $WORKSPACE
+
+curl -s https://raw.github.com/willowgarage/catkin/master/test/test.rosinstall > test.rosinstall
+rosinstall -n src test.rosinstall
+
+rm -rf SCRATCH
+mkdir SCRATCH
+./src/catkin/test/incremental_by_stack_build.sh src SCRATCH
+
+/bin/echo "^^^^^^^^^^^^^^^^^^  catkin-installed-per-stack.sh ^^^^^^^^^^^^^^^^^^^^"
