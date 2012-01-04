c976e305a7b381d1c13deda6641ad5afca31dd71
Troy Straszheim <straszheim@willowgarage.com>
2012-01-04 11:04:20 -0800

    meh

--- dispatch.sh
+++ dispatch.sh
@@ -44,7 +44,7 @@ sudo mkdir -p /var/cache/pbuilder/ccache
 sudo chmod a+w /var/cache/pbuilder/ccache
 
 cat > pbuilder-env.sh <<EOF
-#!/bin/sh -x
+#!/bin/bash -x
 /bin/echo "vvvvvvvvvvvvvvvvvvv  pbuilder-env.sh vvvvvvvvvvvvvvvvvvvvvv"
 export CCACHE_DIR="/var/cache/pbuilder/ccache"
 export PATH="/usr/lib/ccache:${PATH}"
@@ -56,7 +56,7 @@ pwd
 ls -l
 cd $WORKSPACE
 ls -l
-exec "$@"
+exec "\$@"
 EOF
 
 chmod 755 pbuilder-env.sh
