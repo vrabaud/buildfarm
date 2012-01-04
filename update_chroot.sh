5ac419da4bc12f293263ba2e27c217d752dcce1d
Troy Straszheim <straszheim@willowgarage.com>
2012-01-04 12:29:19 -0800

    meh

--- update_chroot.sh
+++ update_chroot.sh
@@ -12,7 +12,7 @@ case $1 in
     fat)
         apt-get install -y wget git-core mercurial subversion \
             ccache lsb-release ccache cmake libopenmpi-dev \
-            libboost-dev libboost-all-dev \
+            libboost-dev libboost-all-dev python-all \
             gccxml python-empy python-yaml python-setuptools \
             openssl sudo liblog4cxx10-dev
         ;;
