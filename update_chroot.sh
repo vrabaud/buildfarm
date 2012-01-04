9138b3acec7a881c8fb8aa794abdec27325f8ada
Troy Straszheim <straszheim@willowgarage.com>
2012-01-03 21:44:49 -0800

    bing

--- update_chroot.sh
+++ update_chroot.sh
@@ -9,6 +9,6 @@ which ccache
 apt-get install -y wget git-core mercurial subversion \
     ccache lsb-release ccache cmake libopenmpi-dev \
     libboost-dev gccxml python-empy python-yaml python-setuptools openssl sudo \
-    liblog4cxx-dev
+    liblog4cxx10-dev
 
 /bin/echo "^^^^^^^^^^^^^^^^^^  update_chroot.sh ^^^^^^^^^^^^^^^^^^^^"
