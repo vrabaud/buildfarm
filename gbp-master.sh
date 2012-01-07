070a72db58d69babd603c623b12968833ac3f2c7
Troy Straszheim <straszheim@willowgarage.com>
2012-01-07 15:02:29 -0800

    actually do something this time.

--- gbp-master.sh
+++ gbp-master.sh
@@ -13,25 +13,12 @@ cd $WORKSPACE
 
 PKG=$1
 
-exit 0
-
-curl -s https://raw.github.com/willowgarage/catkin/master/test/full.rosinstall > full.rosinstall
-rosinstall -n src full.rosinstall
-
-cd src
-rm -f CMakeLists.txt
-ln -s catkin/toplevel.cmake CMakeLists.txt
-cd ..
-rm -rf build
+git clone git://github.com/wg-debs/$PKG.git src
 mkdir build
 cd build
-cmake ../src
-export ROS_TEST_RESULTS_DIR=$WORKSPACE/build/test_results
+cmake ../src -DCMAKE_INSTALL_PREFIX=/opt/ros/fuerte
 make
-make -i test
-$WORKSPACE/build/env.sh $WORKSPACE/src/ros/tools/rosunit/scripts/clean_junit_xml.py
-make install DESTDIR=$(/bin/pwd)/DESTDIR
-
+make install
 
 /bin/echo "^^^^^^^^^^^^^^^^^^  gbp-3rdparty.sh ^^^^^^^^^^^^^^^^^^^^"
 
