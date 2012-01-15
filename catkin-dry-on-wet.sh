3b9c6524fbcb5744761cefcc3737a51fe8e59061
Brian Gerkey <gerkey@willowgarage.com>
2012-01-15 10:11:24 -0800

    ugly hack to protect against kforge errors

--- catkin-dry-on-wet.sh
+++ catkin-dry-on-wet.sh
@@ -12,7 +12,10 @@ fi
 cd $WORKSPACE
 
 curl -s https://raw.github.com/willowgarage/catkin/master/test/full.rosinstall > full.rosinstall
-rosinstall -n src full.rosinstall
+# temporary: protect against kforge auth errors
+cmd="rosinstall -n src full.rosinstall"
+$cmd
+while [[ $? != 0 ]]; do $cmd; done
 
 cd src
 rm -f CMakeLists.txt
@@ -35,7 +38,10 @@ mkdir dry
 cd dry
 curl -s https://raw.github.com/willowgarage/catkin/master/test/unstable/desktop-overlay.rosinstall > desktop-overlay.rosinstall
 curl -s https://raw.github.com/willowgarage/catkin/master/test/unstable/extras.rosinstall > extras.rosinstall
-rosinstall -n . $DESTDIR desktop-overlay.rosinstall extras.rosinstall
+# temporary: protect against kforge auth errors
+cmd="rosinstall -n . $DESTDIR desktop-overlay.rosinstall extras.rosinstall"
+$cmd
+while [[ $? != 0 ]]; do $cmd; done
 curl -s https://raw.github.com/willowgarage/catkin/master/test/unstable/perception_pcl-unstable-build-fix.diff > perception_pcl-unstable-build-fix.diff 
 patch -d perception_pcl -p0 < perception_pcl-unstable-build-fix.diff
 . setup.bash
