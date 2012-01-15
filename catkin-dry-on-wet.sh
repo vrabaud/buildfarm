004927ac932e6e76606ec20e74907eed8fbdfac4
Brian Gerkey <gerkey@willowgarage.com>
2012-01-15 13:23:21 -0800

    debug

--- catkin-dry-on-wet.sh
+++ catkin-dry-on-wet.sh
@@ -38,8 +38,9 @@ cd dryland
 curl -s https://raw.github.com/willowgarage/catkin/master/test/unstable/desktop-overlay.rosinstall > desktop-overlay.rosinstall
 curl -s https://raw.github.com/willowgarage/catkin/master/test/unstable/extras.rosinstall > extras.rosinstall
 # temporary: protect against kforge auth errors
-echo $cmd
 cmd="rosinstall -n --delete-changed-uris . $DESTDIR/usr/local desktop-overlay.rosinstall extras.rosinstall"
+echo $cmd
+ls -la $DESTDIR/usr/local
 while ! $cmd; do echo "Trying again..." ; done
 curl -s https://raw.github.com/willowgarage/catkin/master/test/unstable/perception_pcl-unstable-build-fix.diff > perception_pcl-unstable-build-fix.diff 
 patch -d perception_pcl -p0 < perception_pcl-unstable-build-fix.diff
