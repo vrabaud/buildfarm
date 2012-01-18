4b078fe4480177643355b8c3f0ee563743b71b28
Brian Gerkey <gerkey@willowgarage.com>
2012-01-17 19:09:46 -0800

    fresh start

--- catkin-dry-on-wet.sh
+++ catkin-dry-on-wet.sh
@@ -17,14 +17,15 @@ sudo apt-get install python-pip
 #sudo pip install --upgrade rosinstall
 sudo pip install --upgrade rosdep
 
-curl -s https://raw.github.com/willowgarage/catkin/master/test/full.rosinstall > full.rosinstall
-rosinstall -n --delete-changed-uris src full.rosinstall
+curl -s https://raw.github.com/willowgarage/catkin/master/test/test.rosinstall > test.rosinstall
+rm -rf src
+rosinstall -n --delete-changed-uris src test.rosinstall
 
 cd src
 rm -f CMakeLists.txt
 ln -s catkin/toplevel.cmake CMakeLists.txt
 cd ..
-#rm -rf build
+rm -rf build
 mkdir -p build
 cd build
 DESTDIR=$WORKSPACE/install
