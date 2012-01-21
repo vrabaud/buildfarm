87eb5ba18d0a9da9cfc18cfa44720fec3e2995c9
Ethan <ethan.rublee@gmail.com>
2012-01-20 20:52:34 -0800

    adding a wet debs build script... no dput yet

---
+++ catkin-wet-debs.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -e
+
+set -x
+
+TOP=`pwd`
+WORKSPACE=$TOP/ws
+mkdir -p $WORKSPACE 
+
+if [ ! -e buildfarm/.git ]
+then
+    git clone git://github.com/willowgarage/buildfarm.git
+fi
+
+if [ ! -e catkin/.git ]
+then
+    git clone git://github.com/willowgarage/catkin.git
+fi
+
+PATH=`pwd`/catkin/bin:$PATH
+
+./buildfarm/sanity_check.sh
+
+/bin/echo "vvvvvvvvvvvvvvvvvvv  catkin-workspace-all.sh vvvvvvvvvvvvvvvvvvvvvv"
+
+if [ -z "$WORKSPACE" ] ; then
+    /bin/echo "Don't see no workspace."
+    exit 1
+fi
+
+
+distro_url=https://raw.github.com/willowgarage/rosdistro/master/fuerte.yaml
+curl -s $distro_url > distro.yaml
+
+deburls=$(python -c "import yaml; print '\n'.join([x['url'] for x in yaml.load(open('distro.yaml'))])")
+cd $WORKSPACE
+
+for x in $deburls
+do
+    dirname=$(basename ${x%.git})
+    if [ -e $dirname/.git ]
+    then
+	(cd $dirname && git pull)
+    else
+	git clone $x $dirname
+    fi
+done
+
+
+catkin-build-debs-of-workspace
+
